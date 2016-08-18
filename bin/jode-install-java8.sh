56cec218a7eb116eece0e7f22c0f12ccadb7c342
Michael Andrewes <mandrewes@Bob.local>
2016-08-18 15:40:51 +1000

    none

---
+++ bin/jode-install-java8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm
+
+rpm -Uvh jdk-8u102-linux-x64.rpm
