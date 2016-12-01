0023bb975d68d04fc350ed0e096fd73bd9f82bb4
Michael Andrewes <mandrewes@gmail.com>
2016-12-01 15:00:57 +1100

    none

--- bin/jode-java8-install-centos.sh
+++ bin/jode-java8-install-centos.sh
@@ -6,6 +6,10 @@ mkdir -p $WORK
 
 cd $WORK
 
+#wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u79-b15/jdk-7u79-linux-x64.tar.gz
+
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u102-b14/jdk-8u102-linux-x64.rpm
+
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jce/8/jce_policy-8.zip
 
 rpm -Uvh jdk-8u102-linux-x64.rpm
